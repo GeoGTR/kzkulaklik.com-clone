dc93d3e44fba0af604c91474145ceaff2b285287
Fatih Özgür <fatihozgurrgal@gmail.com>
2021-10-25 00:02:08 +0300

    Navbar düzenlendi Bootstrap ve Fontawesome eklendi

--- app.vue
+++ app.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
     <!-- Remove this component to get started! -->
-    <NuxtWelcome />
+    <link rel="stylesheet" href="./CSS/Style.css">
+    <link rel="stylesheet" href="./fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="./CSS/Bootstrap/bootstrap.min.css">
+    <Navbar/>
+
   </div>
 </template>
+
