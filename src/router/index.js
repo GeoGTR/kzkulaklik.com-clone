fff787c24046aca29fa49e7377b7926cf49b1a26
rau <ttahamurat@gmail.com>
2021-12-11 01:20:31 +0300

    routera lazy loading eklendi

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-import Index from '../views/index.vue'
+/* import Index from '../views/index.vue'
 import Cart from '../views/cart.vue'
 import Contact from '../views/contact.vue'
 import Kulakliklar from '../views/kulakliklar.vue'
@@ -10,77 +9,63 @@ import DistanceSalesAgreement from '../views/distanceSalesAgreementPage'
 import Kablolar from '../views/kablolar.vue'
 import PaymentAndDelivery from '../views/paymentAndDeliveryPage.vue'
 import PrivacyAndSecurity from '../views/privacyAndSecurityPage.vue'
-import WarrantyAndReturn from '../views/warrantyAndReturnPage.vue'
+import WarrantyAndReturn from '../views/warrantyAndReturnPage.vue' */
 
 const routes = [
   {
-    path: '/a',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-
-  {
     path: '/',
     name: 'IndexPage',
-    component: Index
+    component: () => import(/* webpackChunkName: "index" */ '@/views/index.vue')
   },
   {
     path: '/cart',
     name: 'CartPage',
-    component: Cart
+    component: () => import(/* webpackChunkName: "cart" */ '@/views/cart.vue')
   },
   {
     path: '/contact',
     name: 'ContactPage',
-    component: Contact
+    component: () => import(/* webpackChunkName: "contact" */ '@/views/contact.vue')
   },
   {
     path: '/kulakliklar',
     name: 'KulakliklarPage',
-    component: Kulakliklar
+    component: () => import(/* webpackChunkName: "kulakliklar" */ '@/views/kulakliklar.vue')
   },
   {
     path: '/login',
     name: 'LoginPage',
-    component: LoginPage
+    component: () => import(/* webpackChunkName: "login" */ '@/views/loginPage.vue')
   },
   {
     path: '/product',
     name: 'ProductPage',
-    component: Product
+    component: () => import(/* webpackChunkName: "product" */ '@/views/product.vue')
   },
   {
     path: '/distanceSalesAgreement',
     name: 'DistanceSalesAgreementPage',
-    component: DistanceSalesAgreement
+    component: () => import(/* webpackChunkName: "distanceSalesAgreement" */ '@/views/distanceSalesAgreementPage.vue')
   },
   {
     path: '/kablolar',
     name: 'KablolarPage',
-    component: Kablolar
+    component: () => import(/* webpackChunkName: "kablolar" */ '@/views/kablolar.vue')
   },
   {
     path: '/paymentAndDelivery',
     name: 'PaymentAndDeliveryPage',
-    component: PaymentAndDelivery
+    component: () => import(/* webpackChunkName: "paymentAndDelivery" */ '@/views/paymentAndDeliveryPage.vue')
   },
   {
     path: '/privacyAndSecurity',
     name: 'PrivacyAndSecurityPage',
-    component: PrivacyAndSecurity
+    component: () => import(/* webpackChunkName: "privacyAndSecurity" */ '@/views/privacyAndSecurityPage.vue')
   },
   {
     path: '/warrantyAndReturn',
     name: 'WarrantyAndReturnPage',
-    component: WarrantyAndReturn
+    component: () => import(/* webpackChunkName: "warrantyAndReturn" */ '@/views/warrantyAndReturnPage.vue')
   }
 
 ]
