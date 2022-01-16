f41bb4cc1a74a798304fb788e002badd59fb5a17
rau <ttahamurat@gmail.com>
2022-01-16 20:45:21 +0300

    test deneme

--- src/main.js
+++ src/main.js
@@ -25,6 +25,9 @@ const firebaseConfig = {
 
 }
 // Initialize Firebase
-const app = initializeApp(firebaseConfig)
+initializeApp(firebaseConfig)
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+app.use(store)
+app.use(router)
+app.mount('#app')
